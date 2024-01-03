6d1226c1d6d003f6b2bd6c086fc4144d04241615
Tim <tilr20@student.bth.se>
2024-01-03 11:20:45 +0100

    sparar inlogg

--- laxen/FirebaseConfig.js
+++ laxen/FirebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
@@ -15,5 +16,8 @@ const firebaseConfig = {
 
 
   export const FIREBASE_APP = initializeApp(firebaseConfig);
+  initializeAuth(FIREBASE_APP, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  });
   export const FIREBASE_DB = getFirestore(FIREBASE_APP);
   export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
